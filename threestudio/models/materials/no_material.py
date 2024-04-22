df9b97ba30f0bd3b312f7afdac2b2f8bc6ef2596
PanZiqiAI <panziqi20080808@163.com>
2024-04-22 19:00:39 +0800

    study no-material

--- threestudio/models/materials/no_material.py
+++ threestudio/models/materials/no_material.py
@@ -26,30 +26,17 @@ class NoMaterial(BaseMaterial):
 
     def configure(self) -> None:
         self.use_network = False
-        if (
-            self.cfg.input_feature_dims is not None
-            and self.cfg.mlp_network_config is not None
-        ):
-            self.network = get_mlp(
-                self.cfg.input_feature_dims,
-                self.cfg.n_output_dims,
-                self.cfg.mlp_network_config,
-            )
+        if self.cfg.input_feature_dims is not None and self.cfg.mlp_network_config is not None:
+            self.network = get_mlp(self.cfg.input_feature_dims, self.cfg.n_output_dims, self.cfg.mlp_network_config)
             self.use_network = True
         self.requires_normal = self.cfg.requires_normal
 
-    def forward(
-        self, features: Float[Tensor, "B ... Nf"], **kwargs
-    ) -> Float[Tensor, "B ... Nc"]:
+    def forward(self, features: Float[Tensor, "B ... Nf"], **kwargs) -> Float[Tensor, "B ... Nc"]:
         if not self.use_network:
-            assert (
-                features.shape[-1] == self.cfg.n_output_dims
-            ), f"Expected {self.cfg.n_output_dims} output dims, only got {features.shape[-1]} dims input."
+            assert features.shape[-1] == self.cfg.n_output_dims, f"Expected {self.cfg.n_output_dims} output dims, only got {features.shape[-1]} dims input."
             color = get_activation(self.cfg.color_activation)(features)
         else:
-            color = self.network(features.view(-1, features.shape[-1])).view(
-                *features.shape[:-1], self.cfg.n_output_dims
-            )
+            color = self.network(features.view(-1, features.shape[-1])).view(*features.shape[:-1], self.cfg.n_output_dims)
             color = get_activation(self.cfg.color_activation)(color)
         return color
 
@@ -57,7 +44,5 @@ class NoMaterial(BaseMaterial):
         color = self(features, **kwargs).clamp(0, 1)
         assert color.shape[-1] >= 3, "Output color must have at least 3 channels"
         if color.shape[-1] > 3:
-            threestudio.warn(
-                "Output color has >3 channels, treating the first 3 as RGB"
-            )
+            threestudio.warn("Output color has >3 channels, treating the first 3 as RGB")
         return {"albedo": color[..., :3]}

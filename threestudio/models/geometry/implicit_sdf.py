6bd1981de07265a8893042169070b43c70c2506e
PanZiqiAI <panziqi20080808@163.com>
2024-04-23 19:59:04 +0800

    study

--- threestudio/models/geometry/implicit_sdf.py
+++ threestudio/models/geometry/implicit_sdf.py
@@ -60,60 +60,39 @@ class ImplicitSDF(BaseImplicitGeometry):
 
     def configure(self) -> None:
         super().configure()
-        self.encoding = get_encoding(
-            self.cfg.n_input_dims, self.cfg.pos_encoding_config
-        )
-        self.sdf_network = get_mlp(
-            self.encoding.n_output_dims, 1, self.cfg.mlp_network_config
-        )
-
+        self.encoding = get_encoding(self.cfg.n_input_dims, self.cfg.pos_encoding_config)
+        self.sdf_network = get_mlp(self.encoding.n_output_dims, 1, self.cfg.mlp_network_config)
         if self.cfg.n_feature_dims > 0:
-            self.feature_network = get_mlp(
-                self.encoding.n_output_dims,
-                self.cfg.n_feature_dims,
-                self.cfg.mlp_network_config,
-            )
+            self.feature_network = get_mlp(self.encoding.n_output_dims, self.cfg.n_feature_dims, self.cfg.mlp_network_config)
 
         if self.cfg.normal_type == "pred":
-            self.normal_network = get_mlp(
-                self.encoding.n_output_dims, 3, self.cfg.mlp_network_config
-            )
+            self.normal_network = get_mlp(self.encoding.n_output_dims, 3, self.cfg.mlp_network_config)
         if self.cfg.isosurface_deformable_grid:
-            assert (
-                self.cfg.isosurface_method == "mt"
-            ), "isosurface_deformable_grid only works with mt"
-            self.deformation_network = get_mlp(
-                self.encoding.n_output_dims, 3, self.cfg.mlp_network_config
-            )
-
+            assert self.cfg.isosurface_method == "mt", "isosurface_deformable_grid only works with mt"
+            self.deformation_network = get_mlp(self.encoding.n_output_dims, 3, self.cfg.mlp_network_config)
         self.finite_difference_normal_eps: Optional[float] = None
 
     def initialize_shape(self) -> None:
-        if self.cfg.shape_init is None and not self.cfg.force_shape_init:
-            return
-
+        if self.cfg.shape_init is None and not self.cfg.force_shape_init: return
         # do not initialize shape if weights are provided
-        if self.cfg.weights is not None and not self.cfg.force_shape_init:
-            return
-
+        if self.cfg.weights is not None and not self.cfg.force_shape_init: return
         if self.cfg.sdf_bias != 0.0:
-            threestudio.warn(
-                "shape_init and sdf_bias are both specified, which may lead to unexpected results."
-            )
+            threestudio.warn("shape_init and sdf_bias are both specified, which may lead to unexpected results.")
 
+        ################################################################################################################
+        # 计算指定点处的SDF值. (n_points, 3) => (n_points, 1).
+        ################################################################################################################
         get_gt_sdf: Callable[[Float[Tensor, "N 3"]], Float[Tensor, "N 1"]]
         assert isinstance(self.cfg.shape_init, str)
+        # --------------------------------------------------------------------------------------------------------------
+        # Ellipsoid.
+        # --------------------------------------------------------------------------------------------------------------
         if self.cfg.shape_init == "ellipsoid":
-            assert (
-                isinstance(self.cfg.shape_init_params, Sized)
-                and len(self.cfg.shape_init_params) == 3
-            )
+            assert isinstance(self.cfg.shape_init_params, Sized) and len(self.cfg.shape_init_params) == 3
             size = torch.as_tensor(self.cfg.shape_init_params).to(self.device)
 
             def func(points_rand: Float[Tensor, "N 3"]) -> Float[Tensor, "N 1"]:
-                return ((points_rand / size) ** 2).sum(
-                    dim=-1, keepdim=True
-                ).sqrt() - 1.0  # pseudo signed distance of an ellipsoid
+                return ((points_rand / size) ** 2).sum(dim=-1, keepdim=True).sqrt() - 1.0  # pseudo signed distance of an ellipsoid
 
             get_gt_sdf = func
         elif self.cfg.shape_init == "sphere":
@@ -156,21 +135,11 @@ class ImplicitSDF(BaseImplicitGeometry):
                 "-y": np.array([0, -1, 0]),
                 "-z": np.array([0, 0, -1]),
             }
-            if (
-                self.cfg.shape_init_mesh_up not in dirs
-                or self.cfg.shape_init_mesh_front not in dirs
-            ):
-                raise ValueError(
-                    f"shape_init_mesh_up and shape_init_mesh_front must be one of {dirs}."
-                )
+            if self.cfg.shape_init_mesh_up not in dirs or self.cfg.shape_init_mesh_front not in dirs:
+                raise ValueError(f"shape_init_mesh_up and shape_init_mesh_front must be one of {dirs}.")
             if self.cfg.shape_init_mesh_up[1] == self.cfg.shape_init_mesh_front[1]:
-                raise ValueError(
-                    "shape_init_mesh_up and shape_init_mesh_front must be orthogonal."
-                )
-            z_, x_ = (
-                dir2vec[self.cfg.shape_init_mesh_up],
-                dir2vec[self.cfg.shape_init_mesh_front],
-            )
+                raise ValueError("shape_init_mesh_up and shape_init_mesh_front must be orthogonal.")
+            z_, x_ = dir2vec[self.cfg.shape_init_mesh_up], dir2vec[self.cfg.shape_init_mesh_front]
             y_ = np.cross(z_, x_)
             std2mesh = np.stack([x_, y_, z_], axis=0).T
             mesh2std = np.linalg.inv(std2mesh)
@@ -187,29 +156,19 @@ class ImplicitSDF(BaseImplicitGeometry):
             def func(points_rand: Float[Tensor, "N 3"]) -> Float[Tensor, "N 1"]:
                 # add a negative signed here
                 # as in pysdf the inside of the shape has positive signed distance
-                return torch.from_numpy(-sdf(points_rand.cpu().numpy())).to(
-                    points_rand
-                )[..., None]
+                return torch.from_numpy(-sdf(points_rand.cpu().numpy())).to(points_rand)[..., None]
 
             get_gt_sdf = func
 
         else:
-            raise ValueError(
-                f"Unknown shape initialization type: {self.cfg.shape_init}"
-            )
+            raise ValueError(f"Unknown shape initialization type: {self.cfg.shape_init}")
 
         # Initialize SDF to a given shape when no weights are provided or force_shape_init is True
         optim = torch.optim.Adam(self.parameters(), lr=1e-3)
         from tqdm import tqdm
 
-        for _ in tqdm(
-            range(1000),
-            desc=f"Initializing SDF to a(n) {self.cfg.shape_init}:",
-            disable=get_rank() != 0,
-        ):
-            points_rand = (
-                torch.rand((10000, 3), dtype=torch.float32).to(self.device) * 2.0 - 1.0
-            )
+        for _ in tqdm(range(1000), desc=f"Initializing SDF to a(n) {self.cfg.shape_init}:", disable=get_rank() != 0):
+            points_rand = (torch.rand((10000, 3), dtype=torch.float32).to(self.device) * 2.0 - 1.0)
             sdf_gt = get_gt_sdf(points_rand)
             sdf_pred = self.forward_sdf(points_rand)
             loss = F.mse_loss(sdf_pred, sdf_gt)
@@ -222,7 +181,11 @@ class ImplicitSDF(BaseImplicitGeometry):
             broadcast(param, src=0)
 
     def get_shifted_sdf(self, points: Float[Tensor, "*N Di"], sdf: Float[Tensor, "*N 1"]) -> Float[Tensor, "*N 1"]:
-        sdf_bias: Union[float, Float[Tensor, "*N 1"]]
+        """ 在sdf的基础上施加bias，得到最后的sdf.
+        :param points: (..., n_input_dims).
+        :param sdf: (..., 1).
+        :return (..., 1).
+        """
         if self.cfg.sdf_bias == "ellipsoid":
             assert isinstance(self.cfg.sdf_bias_params, Sized) and len(self.cfg.sdf_bias_params) == 3
             size = torch.as_tensor(self.cfg.sdf_bias_params).to(points)
@@ -244,8 +207,7 @@ class ImplicitSDF(BaseImplicitGeometry):
             torch.set_grad_enabled(True)
             points.requires_grad_(True)
 
-        points_unscaled = points  # points in the original scale
-        points = contract_to_unisphere(points, self.bbox, self.unbounded)  # points normalized to (0, 1)
+        points_unscaled, points = points, contract_to_unisphere(points, self.bbox, self.unbounded)  # points normalized to (0, 1)
 
         enc = self.encoding(points.view(-1, self.cfg.n_input_dims))
         sdf = self.sdf_network(enc).view(*points.shape[:-1], 1)
@@ -295,21 +257,38 @@ class ImplicitSDF(BaseImplicitGeometry):
         return output
 
     def forward_sdf(self, points: Float[Tensor, "*N Di"]) -> Float[Tensor, "*N 1"]:
-        points_unscaled = points
-        points = contract_to_unisphere(points_unscaled, self.bbox, self.unbounded)
+        """
+        :param points: (..., n_input_dims).
+        :return (..., 1).
+        """
+        """ 归一化点. (..., n_input_dims). """
+        points_unscaled, points = points, contract_to_unisphere(points, self.bbox, self.unbounded)
+        # 1. 预测SDF. (..., 1).
         sdf = self.sdf_network(self.encoding(points.reshape(-1, self.cfg.n_input_dims))).reshape(*points.shape[:-1], 1)
+        # 2. 进行偏移. (..., 1).
         sdf = self.get_shifted_sdf(points_unscaled, sdf)
+        # Return
         return sdf
 
     def forward_field(self, points: Float[Tensor, "*N Di"]) -> Tuple[Float[Tensor, "*N 1"], Optional[Float[Tensor, "*N 3"]]]:
-        points_unscaled = points
-        points = contract_to_unisphere(points_unscaled, self.bbox, self.unbounded)
+        """
+        :param points: (..., n_input_dims)
+        :return
+            - sdf: 输入points的场值. (..., 1).
+            - deformation: 输入points的形变. (..., 3).
+        """
+        """ 归一化输入点. (..., n_input_dims). """
+        points_unscaled, points = points, contract_to_unisphere(points, self.bbox, self.unbounded)
+        # 1. Encoding. (..., n_hidden_dims).
         enc = self.encoding(points.reshape(-1, self.cfg.n_input_dims))
+        # 2. 预测SDF并偏移. (..., 1).
         sdf = self.sdf_network(enc).reshape(*points.shape[:-1], 1)
         sdf = self.get_shifted_sdf(points_unscaled, sdf)
+        # 3. 预测点的形变. (..., 3).
         deformation: Optional[Float[Tensor, "*N 3"]] = None
         if self.cfg.isosurface_deformable_grid:
             deformation = self.deformation_network(enc).reshape(*points.shape[:-1], 3)
+        # Return
         return sdf, deformation
 
     def forward_level(self, field: Float[Tensor, "*N 1"], threshold: float) -> Float[Tensor, "*N 1"]:
@@ -333,36 +312,19 @@ class ImplicitSDF(BaseImplicitGeometry):
         return out
 
     def update_step(self, epoch: int, global_step: int, on_load_weights: bool = False):
-        if (
-            self.cfg.normal_type == "finite_difference"
-            or self.cfg.normal_type == "finite_difference_laplacian"
-        ):
+        if self.cfg.normal_type == "finite_difference" or self.cfg.normal_type == "finite_difference_laplacian":
             if isinstance(self.cfg.finite_difference_normal_eps, float):
-                self.finite_difference_normal_eps = (
-                    self.cfg.finite_difference_normal_eps
-                )
+                self.finite_difference_normal_eps = self.cfg.finite_difference_normal_eps
             elif self.cfg.finite_difference_normal_eps == "progressive":
                 # progressive finite difference eps from Neuralangelo
                 # https://arxiv.org/abs/2306.03092
                 hg_conf: Any = self.cfg.pos_encoding_config
-                assert (
-                    hg_conf.otype == "ProgressiveBandHashGrid"
-                ), "finite_difference_normal_eps=progressive only works with ProgressiveBandHashGrid"
-                current_level = min(
-                    hg_conf.start_level
-                    + max(global_step - hg_conf.start_step, 0) // hg_conf.update_steps,
-                    hg_conf.n_levels,
-                )
-                grid_res = hg_conf.base_resolution * hg_conf.per_level_scale ** (
-                    current_level - 1
-                )
+                assert hg_conf.otype == "ProgressiveBandHashGrid", "finite_difference_normal_eps=progressive only works with ProgressiveBandHashGrid"
+                current_level = min(hg_conf.start_level + max(global_step - hg_conf.start_step, 0) // hg_conf.update_steps, hg_conf.n_levels)
+                grid_res = hg_conf.base_resolution * hg_conf.per_level_scale ** (current_level - 1)
                 grid_size = 2 * self.cfg.radius / grid_res
                 if grid_size != self.finite_difference_normal_eps:
-                    threestudio.info(
-                        f"Update finite_difference_normal_eps to {grid_size}"
-                    )
+                    threestudio.info(f"Update finite_difference_normal_eps to {grid_size}")
                 self.finite_difference_normal_eps = grid_size
             else:
-                raise ValueError(
-                    f"Unknown finite_difference_normal_eps={self.cfg.finite_difference_normal_eps}"
-                )
+                raise ValueError(f"Unknown finite_difference_normal_eps={self.cfg.finite_difference_normal_eps}")

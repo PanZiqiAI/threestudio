95127b82f89710b617933662138ccbcf2804f687
PanZiqiAI <panziqi20080808@163.com>
2024-04-29 00:22:46 +0800

    modify

--- zscripts-reprod/magic3d-2refine-sd.sh
+++ zscripts-reprod/magic3d-2refine-sd.sh
@@ -19,8 +19,21 @@ P8="Xi_Jinping_is_eating_a_pancake@20240427-234502"
 for PROMPT_DATE in "${P1}" "${P2}" "${P3}" "${P4}" "${P5}" "${P6}" "${P7}" "${P8}" ; do
     PROMPT_=$(echo "${PROMPT_DATE}" | cut -d '@' -f 1)
     PROMPT="${PROMPT_//_/ }"
+
+#    python launch.py --config configs/magic3d-refine-sd.yaml --train --gpu 0 system.renderer.context_type="cuda" \
+#      system.prompt_processor.prompt="${PROMPT}" \
+#      system.geometry_convert_from="${CKPT_ROOT}/${PROMPT_DATE}/ckpts/last.ckpt" \
+#      name="magic3d-refine-sd/baseline"
+
+#    python launch.py --config configs/magic3d-refine-sd.yaml --train --gpu 0 system.renderer.context_type="cuda" \
+#      system.prompt_processor.prompt="${PROMPT}" \
+#      system.geometry_convert_from="${CKPT_ROOT}/${PROMPT_DATE}/ckpts/last.ckpt" \
+#      system.loss.lambda_normal_consistency=100000 \
+#      name="magic3d-refine-sd/lmd_normal_consis=100000"
+
     python launch.py --config configs/magic3d-refine-sd.yaml --train --gpu 0 system.renderer.context_type="cuda" \
       system.prompt_processor.prompt="${PROMPT}" \
       system.geometry_convert_from="${CKPT_ROOT}/${PROMPT_DATE}/ckpts/last.ckpt" \
-      name="magic3d-refine-sd/baseline"
+      system.loss.lambda_normal_consistency=100000 system.guidance.use_img_loss=true system.loss.lambda_sds_img=1.0 \
+      name="magic3d-refine-sd/lmd_normal_consis=100000-use_img_loss@1.0"
 done
